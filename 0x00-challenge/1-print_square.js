a3b7f70788e3556632d1f166a27e623a0195f47f
Lisa Olson <olson.lisa94@gmail.com>
2018-08-03 18:29:26 +0000

    changes the base number

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env nodejs
 /*
     Print a square with the character #
     
@@ -14,10 +14,10 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2])
+size = parseInt(process.argv[2], 10)
 
-for (var i = 0 ; i < size ; i ++) {
-    for (var j = 0 ; j < size ; j ++) {
+for (var i = 0; i < size; i ++) {
+    for (var j = 0; j < size; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
