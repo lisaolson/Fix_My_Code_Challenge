1bd089a7853fe52b2af11ecaf6fcb2616fa10843
Lisa Olson <olson.lisa94@gmail.com>
2018-08-03 18:22:03 +0000

    corrects size of box

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,7 +14,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 16)
+size = parseInt(process.argv[2])
 
 for (var i = 0 ; i < size ; i ++) {
     for (var j = 0 ; j < size ; j ++) {
